f646bc695d49be6aecbb487f312ea28bd9f4f4d8
JmMNDZA <jmmndz3312@gmail.com>
2026-02-19 19:36:19 +0800

    addedFlyonUI

---
+++ app/components/Button.tsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+export function Button() {
+    return (
+    <button className="btn btn-gradient btn-warning">Notes</button>
+        );
+    }
+
+
+export default Button;
