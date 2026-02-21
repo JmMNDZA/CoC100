7e2b6a380d213ff806f3290efc1f2a92a693883e
JmMNDZA <jmmndz3312@gmail.com>
2026-02-21 16:15:48 +0800

    moved coc20

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import { Button } from "./ButtonX";
 import { ValueModify } from "./ValueModify";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import "app/app.css";
 
 export function Navbar() {
   return (
     <div className="flex flex-row items-center justify-evenly m-4 gap-10">
+      <h2 className="text-sm text-accent font-bold">COC20 </h2>
       <h1 id="charName">Character Name</h1>
       <div className= "flex flex-row items-center justify-center gap-10">
       <ValueModify name="Health " placeholder="50 / 50"/>
       <ValueModify name="Magic " placeholder="50 / 50"/>
       <ValueModify name="Sanity " placeholder="50 / 50"/>
       </div>
-      <div className= "flex flex-row items-center justify-center gap-4">
+          <div className= "flex flex-row items-center justify-center gap-4">
       <Button name="Notes" buttonType="notesButton"/>
       <Button name="Save Character" buttonType="jsonButton"/>
       <Button name="Load Character" buttonType="jsonButton"/>
--- app/components/ValueModify.tsx
+++ app/components/ValueModify.tsx
@@ -1,5 +1,3 @@
-import "app/app.css";
-
 export function ValueModify({ name, placeholder }: { name: string; modifyType?: string; optional?: string; placeholder?: string }) {
             return <div className="flex flex-row items-center justify-center gap-4">
                 <label className="text-sm light:text-gray-700 dark:white">{name}</label>
