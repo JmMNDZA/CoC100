15039df338eb2e48c9c4a9853f15d59e62c53bfa
JM Mendoza <jmmndz3312@gmail.com>
2026-02-22 06:49:32 +0800

    minor theme fix

--- app/components/ButtonX.tsx
+++ app/components/ButtonX.tsx
@@ -1,9 +1,9 @@
 export function Button({ name, buttonType }: { name: string; buttonType?: string }) {
         if (buttonType === "jsonButton") {
-            return <button className="btn btn-outline btn-neutral rounded-full">{name}</button>;
+            return <button className="btn btn-neutral rounded-full">{name}</button>;
         }
         else if (buttonType === "notesButton") {
-            return <button className="btn btn-primary btn-soft w-32 rounded-full">{name}</button>;
+            return <button className="btn btn-primary w-32 rounded-full">{name}</button>;
         }
 }
 export default Button;
