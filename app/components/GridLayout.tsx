c7d0ffbebbd3046f9392cacab14cd92555757779
JM Mendoza <jmmndz3312@gmail.com>
2026-02-22 14:35:25 +0800

    final theme change so now ill be working on the other shit

--- app/components/GridLayout.tsx
+++ app/components/GridLayout.tsx
@@ -2,12 +2,12 @@ import { CharInfo } from "./CharInfo"
 
 export function GridLayout() {
     return <div className="grid grid-flow-col grid-cols-[1fr_2fr] grid-rows-5 pr-10 pl-10 gap-4 mr w-full">
-        <div className="bg-base-100 rounded-lg"><CharInfo /></div>
-        <div className="bg-base-100 rounded-lg p-4">Characteristics</div>
-        <div className="bg-base-100 rounded-lg p-4">Combat Info</div>
-        <div className="bg-base-100 rounded-lg p-4">Weapons</div>
-        <div className="bg-base-100 rounded-lg p-4">Inventory</div>
-        <div className="bg-base-100 rounded-lg p-4">Investigator Skills</div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]"><CharInfo /></div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]">Characteristics</div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]">Combat Info</div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]">Weapons</div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]">Inventory</div>
+        <div className="bg-base-100 rounded-[var(--radius-box)]">Investigator Skills</div>
     </div>;
 }
 
