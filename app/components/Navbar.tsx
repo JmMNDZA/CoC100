f64613a8980b96309d29d23485b52b45c2209bab
JM Mendoza <jmmndz3312@gmail.com>
2026-02-24 03:06:02 +0800

    slight navbar gap change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,13 +5,13 @@ import "app/app.css";
 
 export function Navbar() {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap justify-evenly items-center w-full pr-4 pl-4 gap-5 py-5">
-  <div className="flex flex-row gap-2">
-    <img className="object-scale-down h-13 w-13 rounded-full border-primary border-solid border-2" src="https://placehold.net/400x400.png"/>
+    <div className="flex flex-col md:flex-row flex-wrap justify-evenly items-center w-full pr-4 pl-4 gap-x-5 gap-y-2 py-5">
+  <div className="flex flex-row gap-2 items-center justify-center">
     <div className="flex flex-col">
+      <img className="object-scale-down h-13 w-13 rounded-full border-primary border-solid border-2" src="https://placehold.net/400x400.png"/>
       <h3>COC100 </h3>
-      < h1 id="charName" className="mt-[-8px]">Character Name</h1>
     </div>
+      < h1 id="charName" className="pb-5">Character Name</h1>
   </div>
   <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
     <ValueModify name="Health " placeholder="50 / 50" modifyType="true"/>
