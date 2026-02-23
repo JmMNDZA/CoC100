0feb29a4be75894edda49057a22a292517f3749c
JM Mendoza <jmmndz3312@gmail.com>
2026-02-23 14:52:49 +0800

    added luck and movement, also fixed margin/padding with the valuemodify element

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,15 +5,18 @@ import "app/app.css";
 
 export function Navbar() {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap justify-evenly items-center w-full pr-4 pl-4 gap-5 py-8">
+    <div className="flex flex-col md:flex-row flex-wrap justify-evenly items-center w-full pr-4 pl-4 gap-5 py-5">
   <div className="flex flex-col">
     <h3>COC100 </h3>
     <h1 id="charName" className="mt-[-5px]">Character Name</h1>
   </div>
-  <div className="flex flex-row items-center justify-center gap-6 flex-wrap">
-    <ValueModify name="Health " placeholder="50 / 50"/>
-    <ValueModify name="Magic " placeholder="50 / 50"/>
-    <ValueModify name="Sanity " placeholder="50 / 50"/>
+  <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
+    <ValueModify name="Health " placeholder="50 / 50" modifyType="true"/>
+    <ValueModify name="Magic " placeholder="50 / 50" modifyType="true"/>
+    <ValueModify name="Sanity " placeholder="50 / 50" modifyType="true"/>
+    <ValueModify name="MOV" placeholder="50" modifyType="false"/>
+    <ValueModify name="LUCK" placeholder="50" modifyType="false"/>
+
   </div>
   <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
     <Button name="Notes" buttonType="notesButton"/>
--- app/components/ValueModify.tsx
+++ app/components/ValueModify.tsx
@@ -1,7 +1,22 @@
-export function ValueModify({ name, placeholder }: { name: string; modifyType?: string; optional?: string; placeholder?: string }) {
-            return <div className="flex flex-row items-center justify-center gap-4">
-                <label className="text-base light:text-gray-700 dark:white">{name}</label>
-                <input type="text"   className="input w-19 placeholder:!text-black dark:placeholder:!text-white" placeholder={placeholder || "Type here"} />
-            </div>        
+export function ValueModify({ name, placeholder, label, modifyType }: { name: string; label?: string; placeholder?: string, modifyType: string }) {
+    if (modifyType==="true") {
+        return (
+            <fieldset className="flex items-center justify-center border-0 p-0 m-0 min-w-0">
+                <legend className="text-sm font-normal text-center p-0 m-0">{name}</legend>
+                <input type="text" className="input w-20 text-base -mt-0 placeholder-black placeholder:font-bold dark:placeholder-white text-center" placeholder={placeholder}/>
+                <p className="label">{label}</p>
+            </fieldset>
+    );
+    }
+    else if (modifyType==="false") {
+        return (
+            <fieldset className="flex items-center justify-center border-0 p-0 m-0 min-w-0">
+                <legend className="text-sm font-normal text-center p-0 m-0">{name}</legend>
+                <input type="text" className="input w-14 text-base -mt-0 placeholder-black placeholder:font-bold dark:placeholder-white text-center" placeholder={placeholder}/>
+                <p className="label">{label}</p>
+            </fieldset>
+    );
+    }
+
 }
-export default ValueModify; 
+export default ValueModify;
