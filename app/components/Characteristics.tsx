fed0479a2276d116e771e5f3036b89310ac8b048
JM Mendoza <jmmndz3312@gmail.com>
2026-02-25 23:18:07 +0800

    attempt to add full half fifth to grid. not finished

--- app/components/Characteristics.tsx
+++ app/components/Characteristics.tsx
@@ -2,9 +2,25 @@ import { FullHalfFifth } from "./FullHalfFifth"
 
 export function Characteristics() {
     return (
-           <div className="flex flex-col justify-center p-2">
-                    <FullHalfFifth />
-           </div> 
+            <div className="flex flex-col justify-center p-2">
+                <div className="flex aligns-center justify-center text-center">
+                <h2>INFORMATION</h2>
+                </div>
+                <div className="grid grid-cols-2 grid-rows-4 pr-5 pl-5 pb-3 gap-x-3 gap-y-0.8">
+                    <div className="flex flex-col justify-center items-center">
+                        <h3> STR </h3>
+                        <FullHalfFifth />
+                    </div>            
+                    <div className="flex flex-col justify-center items-center">
+                        <h3> DEX </h3>
+                        <FullHalfFifth />
+                    </div>                  
+                    <div className="flex flex-col justify-center items-center">
+                        <h3> POW </h3>
+                        <FullHalfFifth />
+                    </div>                              
+                </div>
+            </div>
     );
 }
 export default Characteristics;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,14 +6,12 @@ import "app/app.css";
 export function Navbar() {
   return (
     <div className="flex flex-col md:flex-row flex-wrap justify-evenly items-center w-full pr-4 pl-4 gap-x-5 gap-y-2 py-5">
-  <div className="flex flex-row gap-2 items-end justify-center">
-    <div className="flex flex-col">
+  <div className="flex flex-col items-end justify-center">
       <img className="object-scale-down h-13 w-13 rounded-full border-primary border-solid border-2" src="https://placehold.net/400x400.png"/>
-      <h3>COC100 </h3>
+      <h3>COC100</h3>
     </div>
-      < h1 id="charName" className="pb-5">Character Name</h1>
-  </div>
-  <div className="flex flex-row items-center justify-center gap-4 flex-wrap">
+      < h1 id="charName">Character Name</h1>
+  <div className="flex flex-row items-center justify-center gap-4 pb-3 flex-wrap">
     <ValueModify name="Health " placeholder="50 / 50" modifyType="true"/>
     <ValueModify name="Magic " placeholder="50 / 50" modifyType="true"/>
     <ValueModify name="Sanity " placeholder="50 / 50" modifyType="true"/>
